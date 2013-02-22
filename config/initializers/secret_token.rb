0cf44932ba287e698d1ab96452362f1cb4612f1e
Stephen Doxsee <stephen@doxsee.org>
2013-02-22 11:36:37 +0000

    initial commit using rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Richlist2::Application.config.secret_token = '6a2e4ab4754d4902f85012b1afb9ad7ab5e3b1e960fa530545363a0983fbd786efd3419963670f160d61e1ee6c36e01a639b1f84c3253463a1bd38fbb9cbf668'
